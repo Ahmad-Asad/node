94acde9dddc7839bd53c2d9444116f48145b724b
Ahmad Asad <34807540+Ahmad-Asad@users.noreply.github.com>
2021-08-09 16:33:13 +0500

    manual upload
    manual upload

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+
+
+app.get('/' , (req , res)=>{
+
+   res.send('hello from simple server :)')
+
+})
+
+app.listen(3000, () => {
+
+    console.log("Hello from Node.JS");
+});
+
+
