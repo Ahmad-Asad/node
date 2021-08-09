3a640f0e29300e2ffdb58ff164579624ea38ea39
Ahmad12506 <asadahmad519@gmail.com>
2021-08-09 17:13:30 +0500

    App.js fille is added

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const express = require('express');
 const app = express();
 
@@ -9,26 +8,8 @@ app.get('/' , (req , res)=>{
 
 })
 
-app.listen(3000, () => {
+const port = process.env.port || 3000;
+app.listen(port, () => {
 
     console.log("Hello from Node.JS");
 });
-
-=======
-const express = require('express');
-const app = express();
-
-
-app.get('/' , (req , res)=>{
-
-   res.send('hello from simple server :)')
-
-})
-
-app.listen(3000, () => {
-
-    console.log("Hello from Node.JS");
-});
-
-
->>>>>>> 94acde9dddc7839bd53c2d9444116f48145b724b
